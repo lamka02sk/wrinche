9165c4d5cf47238d06e31464d5f82620e89b9b2f
lamka02sk <lamka02sk@gmail.com>
2017-08-12 20:17:47 +0200

    - Added support for resuming some more inline components - Small inline components animation changes

--- app/Components/Scripts/note.js
+++ app/Components/Scripts/note.js
@@ -2,6 +2,12 @@ componentsModule.modules.note = {
 
     data: {},
 
+    resumeInline: function(identifier, element, resumeData) {
+
+        componentsModule.modules.note.create(identifier, element, resumeData);
+
+    },
+
     validate: function() {
         return true;
     },
@@ -10,33 +16,36 @@ componentsModule.modules.note = {
         return componentsModule.modules.note.data;
     },
 
-    create: function(identifier, element) {
+    create: function(identifier, element, resumeData) {
 
-        componentsModule.modules.note.data[identifier] = {
-            title: '',
-            note: '',
+        let current       = componentsModule.modules.note;
+        let positionItems = element.querySelectorAll('span.position-item');
+        let textarea      = element.querySelector('textarea[name=component_inline_note]');
+
+        current.data[identifier] = {
+            title        : '',
+            note         : '',
             note_position: 1,
-            disabled: 0
+            disabled     : 0
         };
 
-        let positionItems = element.querySelectorAll('span.position-item');
         componentsModule.initializeEvents([
 
             {
                 // Serialize textarea
-                event: 'change keyup',
-                element: element.querySelector('textarea[name=component_inline_note]'),
+                event  : 'change keyup',
+                element: textarea,
                 content: function(event) {
-                    componentsModule.modules.note.data[identifier].note = event.target.value.trim();
+                    current.data[identifier].note = event.target.value.trim();
                 }
             },
 
             {
                 // Serialize position
-                event: 'click',
+                event  : 'click',
                 element: positionItems,
                 content: function(event) {
-                    componentsModule.modules.note.data[identifier].note_position = event.target.getAttribute('data-position');
+                    current.data[identifier].note_position = event.target.getAttribute('data-position');
                     positionItems.forEach(function(item) {
                         item.classList.remove('active');
                     });
@@ -45,6 +54,20 @@ componentsModule.modules.note = {
             }
 
         ]);
+
+        if(resumeData) {
+
+            if(resumeData.note) {
+
+                textarea.value = resumeData.note.trim();
+                triggerEvent(textarea, 'change');
+
+            }
+
+            if(resumeData.note_position !== undefined)
+                positionItems[resumeData.note_position].click();
+
+        }
 
     }
 
